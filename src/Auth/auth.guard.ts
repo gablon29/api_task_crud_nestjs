a9c223f48d0169bff42ec321435d8ab1827656bb
gablon29 <gabriellondero40@gmail.com>
2025-02-03 18:02:48 -0300

    feat: remove console log from AuthGuard and enhance token validation in AuthService

--- src/Auth/auth.guard.ts
+++ src/Auth/auth.guard.ts
@@ -25,7 +25,6 @@ export class AuthGuard implements CanActivate {
     if (!validate) {
       throw new UnauthorizedException('Invalid token');
     }
-    console.log('Token validated');
     return true;
   }
 }
